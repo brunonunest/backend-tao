314ec8043e53e2e4bb0f82d3c221a3e836f7ff8e
brunonunest <brunonunest@gmail.com>
2020-10-14 17:10:52 -0300

    admin ok

--- organizer/admin.py
+++ organizer/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 
 from django.contrib.admin import ModelAdmin
-from .models import Tag
+from .models import Tag, Enterprise
+from django.utils.translation import gettext_lazy as _
+
 
 @admin.register(Tag)
 class TagAdmin(ModelAdmin):
-    search_fields = ("name",)
+    search_fields = ("name", "slug",)
     list_display = ("name", "slug",)
-    list_filter = ("name", "slug",)
+
+
+@admin.register(Enterprise)
+class EnterpriseAdmin(ModelAdmin):
+    search_fields = ("name", "slug", "tags__slug", "tags__name",)
+    list_display = ("name", "slug", "description", "founded_date", "contact", "website",)
+    list_filter = ("tags",)
+    fieldsets = (
+        (None, {"fields": ("name", "slug", "description",)}),
+        (_("Data"), {"fields": ("founded_date", "contact", "website", "tags",)}),
+    )
+adm
